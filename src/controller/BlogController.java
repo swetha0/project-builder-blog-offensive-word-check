5a834bf52362a4851ee33a5d06622159948ae054
swetha0 <gaali.swetha@gmail.com>
2020-05-14 22:56:09 -0400

    done

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Blog;
 import model.User;
+import utility.CheckBlogPost;
 
 
 @WebServlet(urlPatterns= {"/blog"})
@@ -47,8 +48,10 @@ public class BlogController extends HttpServlet {
 		blog.setBlogTitle(title);
 		blog.setBlogDescription(description);
 		blog.setDate(postedOn);
-
-		if(checkblog!) {
+		
+		CheckBlogPost checkblg = new CheckBlogPost();
+		boolean checkblog = checkblg.checkBlog(blog);
+		if(checkblog!=false) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,72 @@
+/*package utility;
+
+import java.util.Arrays;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface
+{
+	String[] badwords = {"ahole","bitch","cock","dick","erotic","fcuk","gay","heroin","injun","jerk","kill","lesbo","murder","niggle","orally","pawn","quicky","racy","sadist","teez","uterus","virgin","weed","xxx","yobbo","zibbi"};
+	public boolean checkBlogTitle(Blog blog) {
+		boolean contains = Arrays.stream(badwords).anyMatch(blog.getBlogTitle()::equals);
+		contains = !contains;
+		return contains;
+	}
+
+	public boolean checkBlogDescription(Blog blog) {
+		boolean val = Arrays.stream(badwords).anyMatch(blog.getBlogDescription()::equals);
+		val = !val;
+		return val;
+	}
+
+	public boolean checkBlog(Blog blog) {
+		boolean flag = (checkBlogTitle(blog) && checkBlogDescription(blog));
+		return flag;
+	}
+	
+}
+*/
+
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface
+{
+
+	String[] bad_words = {"ahole","bitch","cock","dick","erotic","fcuk","gay","heroin","injun","jerk","kill","lesbo","murder","niggle","orally","pawn","quicky","racy","sadist","teez","uterus","virgin","weed","xxx","yobbo","zibbi"};
+	@Override
+	public boolean checkBlog(Blog blog) {
+		
+		if (checkBlogTitle(blog) && checkBlogDescription(blog))
+			return true;
+		else
+			return false;
+	}
+	
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		
+		String title = blog.getBlogTitle();
+		int i=0;
+		while(i < bad_words.length)
+		{
+			if(title.contains(bad_words[i]))
+				return false;
+		}
+		return true;	
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		String desc = blog.getBlogDescription();
+		int i=0;
+		while(i < bad_words.length)
+		{
+			if(desc.contains(bad_words[i]))
+				return false;
+		}
+		return true;
+	}
+	
+}
